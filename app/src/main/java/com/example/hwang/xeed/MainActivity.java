4a5a5e1163fdc040161227e3fe5dbb60dfd2070b
Hwang <hnaras@naver.com>
2016-08-18 00:13:39 +0900

    바로 전화 걸림 클래스 삭제

--- app/src/main/java/com/example/hwang/xeed/MainActivity.java
+++ app/src/main/java/com/example/hwang/xeed/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hwang.xeed;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -10,15 +11,23 @@ import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends android.app.Activity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends Activity {
 
     Button submitBtn = null;
     EditText nameText = null;
     private ProgressDialog pDialog;
     Cursor cursor;
     int counter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,9 @@ public class MainActivity extends android.app.Activity {
 
         nameText = (EditText) findViewById(R.id.nameText);
         submitBtn = (Button) findViewById(R.id.call);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -47,12 +59,9 @@ public class MainActivity extends android.app.Activity {
             @Override
             public void run() {
                 String phone = getContacts(name);
-
                 pDialog.cancel();
-                if (phone == null) {
-                    Toast.makeText(getApplicationContext(), "sss", Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + phone));
+                if (phone != null) {
+                    Intent myIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone));
                     startActivity(myIntent);
                 }
             }
@@ -70,19 +79,19 @@ public class MainActivity extends android.app.Activity {
         String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
         String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
         ContentResolver contentResolver = getContentResolver();
-        cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+        cursor = contentResolver.query(CONTENT_URI, null, null, null, null);
 
         // Iterate every contact in the phone
         if (cursor.getCount() > 0) {
             counter = 0;
             while (cursor.moveToNext()) {
 
-                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
-                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
-                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
+                String contact_id = cursor.getString(cursor.getColumnIndex(_ID));
+                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
                 if (hasPhoneNumber > 0) {
                     //This is to read multiple phone numbers associated with the same contact
-                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[]{contact_id}, null);
                     while (phoneCursor.moveToNext()) {
                         phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
 
@@ -99,5 +108,45 @@ public class MainActivity extends android.app.Activity {
 
     private boolean isMatchUserName(String name, String paramName) {
         return name.toLowerCase().indexOf(paramName.toLowerCase()) >= 0 ? true : false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.hwang.xeed/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.hwang.xeed/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
