04a7ec6b2f5566daab1be430ad0835ea5652daaa
Hwang <hnaras@naver.com>
2016-08-18 23:33:17 +0900

    TCA Annotation 작업중...

--- app/src/main/java/com/example/hwang/xeed/hmm/CallAction.java
+++ app/src/main/java/com/example/hwang/xeed/hmm/CallAction.java
@@ -1,5 +1,12 @@
 package com.example.hwang.xeed.hmm;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+import cooker.core.annotations.CookerAction;
+import cooker.core.annotations.CookerSerializable;
 import cooker.core.annotations.CookingIngredient;
 
 /**
@@ -8,5 +15,18 @@ import cooker.core.annotations.CookingIngredient;
 @CookingIngredient
 public class CallAction {
 
+    @CookerSerializable(key = "phoneNumber")
+    String phoneNumber;
+    @CookerSerializable(key = "activity")
+    Activity activity;
 
+    @CookerAction
+    public void callAction() {
+        if (phoneNumber == null) {
+            Toast.makeText(activity.getApplicationContext(), "sss", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + phoneNumber));
+            activity.startActivity(myIntent);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/hwang/xeed/hmm/FindTrigger.java
@@ -0,0 +1,12 @@
+package com.example.hwang.xeed.hmm;
+
+import cooker.core.annotations.CookingIngredient;
+
+/**
+ * Created by Hwang on 2016-08-18.
+ */
+@CookingIngredient
+public class FindTrigger {
+
+
+}
